827ae9fa1e7c23531918f18ea6c78914f5dfbd96
dhx1994 <2369014761@qq.com>
2019-07-25 12:33:49 +0800

    第二次更改

--- panduan.py
+++ panduan.py
@@ -2,8 +2,8 @@ b={"name":"张三",
 "sex":"男",
 "age":26,
 "high":173}
-if zidian.get("name") == "张三"：
-    print("张三的年龄是"，zidian["age"])
+if b.get("name") == "张三":
+    print("张三的年龄是",b["age"])
 else:
     print("字典里没有张三！")
     
